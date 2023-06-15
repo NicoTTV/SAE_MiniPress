b86ae97029eb4ef2e40d2f643957afe6c99eb16e
Kuenemann Nicolas <kuenemannnicolas@gmail.com>
2023-06-15 16:10:51 +0200

    Création des routes et classes php

---
+++ minipress.appli/src/actions/GetConnexionAction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace minipress\app\actions;
+
+use Slim\Exception\HttpInternalServerErrorException;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+use Slim\Views\Twig;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class GetConnexionAction extends AbstractAction
+{
+
+    public function __invoke(Request $rq, Response $rs, array $args): Response
+    {
+
+        $twig = Twig::fromRequest($rq);
+        try {
+            return $twig->render($rs, 'user/connexion.twig');
+        } catch (LoaderError|RuntimeError|SyntaxError $e) {
+            throw new HttpInternalServerErrorException($rq, $e->getMessage());
+        }
+    }
+}
---
+++ minipress.appli/src/actions/PostConnexionAction.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace minipress\app\actions;
+
+use minipress\app\services\exceptions\BadDataUserException;
+use minipress\app\services\exceptions\UserNotFoundException;
+use minipress\app\services\user\UserService;
+use Slim\Exception\HttpInternalServerErrorException;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+class PostConnexionAction extends AbstractAction
+{
+
+    public function __invoke(Request $rq, Response $rs, array $args): Response
+    {
+        $data = $rq->getParsedBody();
+        $userService = new UserService();
+        try {
+            $userService->connexion($data);
+        } catch (BadDataUserException|UserNotFoundException $e) {
+            throw new HttpInternalServerErrorException($rq, $e->getMessage());
+        }
+        return $rs->withHeader('Location', '/')->withStatus(200);
+    }
+}
