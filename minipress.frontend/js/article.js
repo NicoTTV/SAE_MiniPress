4389040f1c35d903fc24e9e654cebfb6f2958767
Vivien <vivien.stemmelen2@etu.univ-lorraine.fr>
2023-06-20 10:46:27 +0200

    ajout id categories

--- minipress.frontend/js/article.js
+++ minipress.frontend/js/article.js
@@ -56,6 +56,7 @@ export function updateArticleList(categoryId) {
     if (categoryId === 'all') {
         displayArticles(articles); // Afficher tous les articles
     } else {
+        console.log(categoryId);
         // Récupérer les articles de la catégorie sélectionnée
         fetch(`http://localhost:41004/api/categories/${categoryId}/articles`)
             .then(response => response.json())
