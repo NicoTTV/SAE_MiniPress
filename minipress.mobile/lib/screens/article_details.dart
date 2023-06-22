6e559b4fad698949dd92bf69bac2b9306355af92
Nicolas Kuenemann <nicolas.kuenemann3@etu.univ-lorraine.fr>
2023-06-22 15:26:07 +0200

    merge

--- minipress.mobile/lib/screens/article_details.dart
+++ minipress.mobile/lib/screens/article_details.dart
@@ -3,7 +3,6 @@ import 'package:flutter/widgets.dart';
 import 'package:minipress_mobile/models/article_provider.dart';
 import 'package:provider/provider.dart';
 import '../models/article.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
 
 class ArticleDetails extends StatefulWidget {
   final Article article;
@@ -20,54 +19,70 @@ class _ArticleDetails extends State<ArticleDetails> {
     Provider.of<ArticleProvider>(context, listen: false)
         .fetchDetailsArticle(widget.article.lienDetails['self']['href']);
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: const Text('Détails de l\'article'),
         ),
-        body: Consumer<ArticleProvider>(
-          builder: (BuildContext context, articleProvider, child) {
-            return FutureBuilder(
-                future: articleProvider.readArticle(),
-                builder: ((context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Column(
-                      children: [
-                        Text(
-                          snapshot.data['titre'],
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 48),
-                        ),
-                        Column(
-                          children: [
-                            const Text(
-                              'Résumé de l\'article :',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 24),
+        body: SingleChildScrollView(
+          child: Consumer<ArticleProvider>(
+            builder: (BuildContext context, articleProvider, child) {
+              return FutureBuilder(
+                  future: articleProvider.readArticle(),
+                  builder: ((context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        children: [
+                          const SizedBox(height: 20),
+                          Container(
+                            decoration: const BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.black))),
+                            child: Text(
+                              snapshot.data['titre'],
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 48),
                             ),
-                            MarkdownBody(data: snapshot.data['resume']),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            const Text(
-                              'Contenu :',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 24),
-                            ),
-                            MarkdownBody(
-                              data: snapshot.data['contenu'],
-                            ),
-                          ],
-                        ),
-                        Text(
-                            ' - Article écrit le ${snapshot.data['date_de_creation']}',
-                            style: const TextStyle(fontSize: 12))
-                      ],
-                    );
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                }));
-          },
+                          ),
+                          const SizedBox(height: 40),
+                          Column(
+                            children: [
+                              const Text(
+                                'Résumé de l\'article :',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 24),
+                              ),
+                              Text(
+                                snapshot.data['resume'],
+                                style: const TextStyle(fontSize: 18),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          Column(
+                            children: [
+                              const Text(
+                                'Contenu :',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 24),
+                              ),
+                              Text(
+                                snapshot.data['contenu'],
+                                style: const TextStyle(fontSize: 18),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 50),
+                          Text(
+                              ' - Article écrit le ${snapshot.data['date_de_creation']}',
+                              style: const TextStyle(fontSize: 12))
+                        ],
+                      );
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  }));
+            },
+          ),
         ));
   }
 }
